7797a59739a4eaccea276ab634975f6130a496ac
marilau <tonyfaros@gmail.com>
2018-03-28 00:35:03 -0600

    add fb login in process

--- app/src/main/java/com/example/anthony_pc/pocketrecipe/MainActivity.java
+++ app/src/main/java/com/example/anthony_pc/pocketrecipe/MainActivity.java
@@ -2,15 +2,59 @@ package com.example.anthony_pc.pocketrecipe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.crashlytics.android.answers.Answers;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Answers());
         setContentView(R.layout.activity_main);
+        mQueue = Volley.newRequestQueue(this);
+        parseJson("https://moviles-backoffice.herokuapp.com/persona/?format=json");
+    }
+
+    public void parseJson(String url){
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,
+                url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            Log.e("parse",String.valueOf(response.getJSONObject(0).getString("id")));
+                            Log.e("parse",String.valueOf(response.getJSONObject(0).getString("nombre")));
+                            Log.e("parse",String.valueOf(response.getJSONObject(0).getString("correo")));
+                            Log.e("parse",String.valueOf(response.getJSONObject(0).getString("contrasena")));
+                            //String bla = response.getJSONObject("quotes").get("USDCRC").toString();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(jsonArrayRequest);
+
     }
 }
