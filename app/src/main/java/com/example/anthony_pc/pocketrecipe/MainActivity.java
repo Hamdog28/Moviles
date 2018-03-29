cfc99ef46137314bc3f03205195b490fb8768ff5
marilau <tonyfaros@gmail.com>
2018-03-29 01:53:48 -0600

    facebook login

--- app/src/main/java/com/example/anthony_pc/pocketrecipe/MainActivity.java
+++ app/src/main/java/com/example/anthony_pc/pocketrecipe/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.anthony_pc.pocketrecipe;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Call;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -14,24 +18,66 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.crashlytics.android.answers.Answers;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 
 import io.fabric.sdk.android.Fabric;
 
 
 public class MainActivity extends AppCompatActivity {
     private RequestQueue mQueue;
-
+    private LoginButton loginButton;
     TextView titulo;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Answers());
         setContentView(R.layout.activity_main);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setReadPermissions("email","public_profile");
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Toast.makeText(getApplicationContext(),"nice",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(),RegistroActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields","first_name, last_name, email, id");
+                graphRequest.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
 
 
         Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/greatvibes_regular.ttf");
@@ -54,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
                             Log.e("parse",String.valueOf(response.getJSONObject(0).getString("nombre")));
                             Log.e("parse",String.valueOf(response.getJSONObject(0).getString("correo")));
                             Log.e("parse",String.valueOf(response.getJSONObject(0).getString("contrasena")));
-                            //String bla = response.getJSONObject("quotes").get("USDCRC").toString();
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
