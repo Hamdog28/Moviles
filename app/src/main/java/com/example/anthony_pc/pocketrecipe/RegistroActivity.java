0c6b5b8e6ff73b1c849ef2395fcde4226bb12394
Maria Laura Pizarro <marilau63@gmail.com>
2018-03-28 23:57:07 -0600

    UI registro completo

--- app/src/main/java/com/example/anthony_pc/pocketrecipe/RegistroActivity.java
+++ app/src/main/java/com/example/anthony_pc/pocketrecipe/RegistroActivity.java
@@ -1,7 +1,9 @@
 package com.example.anthony_pc.pocketrecipe;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class RegistroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/greatvibes_regular.ttf");
+        TextView titulo = findViewById(R.id.titulo);
+        titulo.setTypeface(custom_font);
+
+        Typeface font = Typeface.createFromAsset( getAssets(), "fontawesome-webfont.ttf" );
+        TextView atras = findViewById(R.id.atras);
+        atras.setTypeface(font);
+
     }
 }
