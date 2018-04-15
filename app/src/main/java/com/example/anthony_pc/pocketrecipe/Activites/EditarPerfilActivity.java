50d4f6f737fc543ba0be76200513fd582aafa71e
Maria Laura Pizarro <marilau63@gmail.com>
2018-04-15 13:27:57 -0600

    editar perfil no sirve lo de subir imagen

--- app/src/main/java/com/example/anthony_pc/pocketrecipe/Activites/EditarPerfilActivity.java
+++ app/src/main/java/com/example/anthony_pc/pocketrecipe/Activites/EditarPerfilActivity.java
@@ -1,11 +1,39 @@
 package com.example.anthony_pc.pocketrecipe.Activites;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.anthony_pc.pocketrecipe.R;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class EditarPerfilActivity extends AppCompatActivity {
+    private static final int GALLERY_INTENT = 1;
+    ImageButton imagen;
+    private static final int RESULT_LOAD_IMAGE = 0;
+    private static final int PERMISSION_REQUEST = 0;
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +41,47 @@ public class EditarPerfilActivity extends AppCompatActivity {
         setContentView(R.layout.activity_editar_perfil);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle("Editar Perfil");
+        imagen = (ImageButton)findViewById(R.id.imagen_button);
+
+        int permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
     }
     @Override
     public boolean onSupportNavigateUp(){
         finish();
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case RESULT_LOAD_IMAGE:
+                if(resultCode == RESULT_OK){
+                    Uri imagenSeleccionada = data.getData();
+                    String[] filePath = {MediaStore.Images.Media.DATA};
+                    Cursor cursor = getContentResolver().query(imagenSeleccionada,filePath,null,null,null);
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex(filePath[0]);
+                    String picturePath = cursor.getString(columnIndex);
+                    cursor.close();
+                    Bitmap bitmap = BitmapFactory.decodeFile(picturePath);
+                    Bitmap resized = Bitmap.createScaledBitmap(bitmap, 100, 100, true);
+                    imagen.setImageBitmap(resized);
+                    Log.e("IMAGENNN","IMAGEN");
+                }
+        }
+    }
+    public void agarrarImagenGaleria(View view){
+        Intent galeriaIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galeriaIntent,RESULT_LOAD_IMAGE);
     }
 }
